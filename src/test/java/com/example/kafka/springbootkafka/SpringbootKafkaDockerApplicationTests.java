bdebdf3fcb58fdb9aef8e297fdabc998f1dfd276
Huy Tran <viethuy99lc@gmail.com>
2021-08-12 07:40:38 +0700

    1

---
+++ src/test/java/com/example/kafka/springbootkafka/SpringbootKafkaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
